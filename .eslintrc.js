c3278016d375549510e85cdb9a7ee8780fd39e65
roydi-projects <40473250+roydi-projects@users.noreply.github.com>
2021-07-04 17:17:06 +0300

    style: fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: "module",
-    },
-    plugins: ["react"],
-    rules: {
-        "no-console": "warn",
-        "import/first": "error",
-        "react/prop-types": 0,
-        "linebreak-style": ["error", "unix"],
-        "prettier/prettier": "error",
-        "linebreak-style": ["error", "unix"],
-        "arrow-body-style": "off",
-        "prefer-arrow-callback": "off",
-        "react/react-in-jsx-scope": "off"
-    },
-}
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    // we only want single quotes
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: [2, 2, { SwitchCase: 1 }],
+    // we want to avoid useless spaces
+    'no-multi-spaces': ['error'],
+    'no-unused-vars': 1,
+    'no-debugger': 1,
+  },
+};
